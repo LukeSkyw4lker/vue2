f826f4727c8738a08072411a72189c2ba02d6bbc
zqn <3060707139@qq.com>
2023-09-10 13:50:41 +0800

    zqn

--- src/views/videos.vue
+++ src/views/videos.vue
@@ -6,7 +6,7 @@
         <a href="#">
           <img src="../assets/img/社区图片1.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV18E411d7NX?t=21.7" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV18E411d7NX?t=21.7" class="play" target="_blank"></a>
         <div class="name">
           <i>社区宣传片...</i> <br>
           <span>光</span>
@@ -17,7 +17,7 @@
         <a href="#">
           <img src="../assets/img/社区图片2.png" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1yD4y1A7f7?t=1.7" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1yD4y1A7f7?t=1.7" class="play" target="_blank"></a>
         <div class="name">
           <i>党员进社区，服务暖人心...</i> <br>
           <span>Carver_1</span>
@@ -28,7 +28,7 @@
         <a href="#">
           <img src="../assets/img/社区图片3.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1eK411K7oB?t=2.0" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1eK411K7oB?t=2.0" class="play" target="_blank"></a>
         <div class="name">
           <i>mg公益动画宣传片</i> <br>
           <span>翼虎动画</span>
@@ -39,7 +39,7 @@
         <a href="#">
           <img src="../assets/img/社区图片4.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1Wg411P7zs?t=1.0" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1Wg411P7zs?t=1.0" class="play" target="_blank"></a>
         <div class="name">
           <i>绿化宣传片...</i> <br>
           <span>用户131411231</span>
@@ -50,7 +50,7 @@
         <a href="#">
           <img src="../assets/img/社区图片5.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1DB4y1y7mT?t=5.4" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1DB4y1y7mT?t=5.4" class="play" target="_blank"></a>
         <div class="name">
           <i>高档小区宣传片...</i> <br>
           <span>小鹿</span>
@@ -61,7 +61,7 @@
         <a href="#">
           <img src="../assets/img/社区图片6.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1XD4y1W71m?t=2.8" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1XD4y1W71m?t=2.8" class="play" target="_blank"></a>
         <div class="name">
           <i>社区活动视频制作...</i> <br>
           <span>用户3342442</span>
@@ -72,7 +72,7 @@
         <a href="#">
           <img src="../assets/img/社区视频7.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1Hu411X7QD?t=1.8" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1Hu411X7QD?t=1.8" class="play" target="_blank"></a>
         <div class="name">
           <i>志愿服务我先行...</i> <br>
           <span>用户532233</span>
@@ -83,7 +83,7 @@
         <a href="#">
           <img src="../assets/img/社区图片8.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1ra4y1h785?t=2.2" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1ra4y1h785?t=2.2" class="play" target="_blank"></a>
         <div class="name">
           <i>义无反顾志愿服务...</i> <br>
           <span>用户5342123</span>
@@ -94,7 +94,7 @@
         <a href="#">
           <img src="../assets/img/社区图片9.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1dw411Z7VJ?t=2.7" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1dw411Z7VJ?t=2.7" class="play" target="_blank"></a>
         <div class="name">
           <i>智能社区健身服务...</i> <br>
           <span>用户63434343</span>
@@ -105,7 +105,7 @@
         <a href="#">
           <img src="../assets/img/社区图片10.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1AT4y1E7Bq?t=12.6" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1AT4y1E7Bq?t=12.6" class="play" target="_blank"></a>
         <div class="name">
           <i>创腾科普 | iLabPower创新</i> <br>
           <span>用户53434343</span>
@@ -116,7 +116,7 @@
         <a href="#">
           <img src="../assets/img/社区图片11.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1VB4y1X7sk?t=1.5" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1VB4y1X7sk?t=1.5" class="play" target="_blank"></a>
         <div class="name">
           <i>社区老人趣味活动...</i> <br>
           <span>用户5343435</span>
@@ -127,7 +127,7 @@
         <a href="#">
           <img src="../assets/img/社区图片12.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1Xh411t7x3?t=1.6" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1Xh411t7x3?t=1.6" class="play" target="_blank"></a>
         <div class="name">
           <i>养老服务圈</i> <br>
           <span>用户7423232</span>
@@ -138,7 +138,7 @@
         <a href="#">
           <img src="../assets/img/社区图片13.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1oG4y1u7a2?t=1.6" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1oG4y1u7a2?t=1.6" class="play" target="_blank"></a>
         <div class="name">
           <i>垃圾分类我先行...</i> <br>
           <span>用户435353</span>
@@ -149,7 +149,7 @@
         <a href="#">
           <img src="../assets/img/社区图片14.jpg" alt="">
         </a>
-        <a href="https://www.bilibili.com/video/BV1Zr4y1A7b1?t=1.6" class="play"></a>
+        <a href="https://www.bilibili.com/video/BV1Zr4y1A7b1?t=1.6" class="play" target="_blank"></a>
         <div class="name">
           <i>小区垃圾房...</i> <br>
           <span>用户23323</span>
